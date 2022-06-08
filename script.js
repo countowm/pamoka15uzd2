c9dd3f8130e44b76a36910f32c306d967b739fab
countowm <countowm@gmail.com>
2022-06-08 14:18:07 +0300

    padariau

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    let zodis = "medis";
+    let zodziai = SudetiZodis(zodis);
+    console.log(SudetiZodis(zodis));
+}
+function SudetiZodis(zodis){
+    return zodis + zodis;
+}
