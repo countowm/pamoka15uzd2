4619364832d0dd59f301a7ef3bad704952092fd2
countowm <countowm@gmail.com>
2022-06-15 12:46:59 +0300

    padariau

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 window.onload = function(){
     let zodis = "medis";
+    let zodis2 = "berzas";
+    let zodis3 = "azuolas";
     let zodziai = SudetiZodis(zodis);
-    console.log(SudetiZodis(zodis));
+    let zodziai2 = SudetiZodis(zodis2);
+    let zodziai3 = SudetiZodis(zodis3);
+    console.log(zodziai);
+    console.log(zodziai2);
+    console.log(zodziai3);
 }
 function SudetiZodis(zodis){
     return zodis + zodis;
